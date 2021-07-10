184b975c0594af90f56747e9b77ad4fcf5a4d72a
dimkk <dima.v@dimkk.be>
2021-07-11 02:31:05 +0300

    - working with test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,66 @@
-console.log("Hello World 👋");
+const convert = (text: string, delimeter = '\r\n') => {
+    let data: any = {};
+    const splitText = text.split(delimeter);
+    splitText.map((t) => {
+        formObject(t, data);
+    });
+    return data;
+};
+
+export default convert;
+
+const isNeededListItem = (str: string): boolean => {
+    const regexp = /^(\d+\.)+(\s|\d\|\.)/;
+    return regexp.exec(str) != null;
+};
+
+const formObject = (str: string, result: any): any => {
+    if (!str) return;
+    str = str.trim();
+    const checkResult = isNeededListItem(str);
+    if (checkResult) {
+        result.currentPath = getPath(str);
+        const text = getText(str);
+        deepNestKeysValues(0, result, result.currentPath, text);
+    } else {
+        if (!result.currentPath) result.currentPath = [];
+        if (result.currentPath.length === 0) {
+            result['header'] = result['header'] || '';
+            result['header'] += str;
+        } else {
+            deepNestKeysValues(0, result, result.currentPath, getText(str));
+        }
+    }
+};
+
+function deepNestKeysValues(p: any, localobj: any, currentPath: any, value?: string) {
+    localobj[currentPath[p]] = localobj[currentPath[p]] || {};
+    if (p === currentPath.length - 1) {
+        localobj[currentPath[p]]['text'] = localobj[currentPath[p]]['text'] || '';
+        localobj[currentPath[p]]['text'] += value;
+        return;
+    }
+    deepNestKeysValues(p + 1, localobj[currentPath[p]], currentPath, value);
+}
+
+function isNumeric(str: string) {
+    if (typeof str != 'string') return false; // we only process strings!
+    return !isNaN(parseFloat(str));
+}
+
+function getPath(str: string): any {
+    let result = [];
+    // assuming we get 1.2.3. [some text] or X.[X.] [some text] or [some text] as str
+    const split = str.split('. ');
+    const numbers = split[0].split('.');
+    result = numbers.map((n) => {
+        if (isNumeric(n)) return n;
+    });
+    return result;
+}
+
+function getText(str: string) {
+    const split = str.split('. ');
+    if (split.length > 1) return split[1];
+    else return split[0];
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,46 @@
-describe("index", () => {
-  it("prints to the console", async () => {
-    expect.assertions(2);
+import ol2json from '../src/index';
 
-    const log = jest.fn();
-    const _ = console.log;
-    console.log = log;
-    try {
-      require("../src/index");
+describe('integration, test whole convert process', () => {
+    const testString = `
+      GGG: Please allow white sockets to meet trade site \r\n socket requirements when selected. If \r\n I ask for 6L blue, some of those being white would still meet the criteria.\r\n
+      1. A checkbox "allow substitution with white" for socket color search would be even cooler.\r\n
+      1.1. But why? Why would I need that? Why would I ever uncheck that box? Is there any (except for 2-3 specific irrelevant uniques) reason why I would rather have colored sockets instead of whites?\r\n
+      1.1.1. The few items that change stat according to socket colours\r\n
+      1.1.2. Addicted to Jousis voice\r\n
+      1.2. Im holding on to the hope that some future unique will be relevant? Choice is always good.\r\n
+      1.3. I mean sure, but given that almost nobody would ever NOT tick the box, it should rather be\r\n
+      "disallow substitution with white" for the edge cases. Otherwise it would just mean\r\n
+      more unnecessary clicking per search.\r\n
+      2. I personally prefer ticking something if it applies, instead of unticking if it doesn't, but yes that would absolutely work too.\r\n
+    `;
+    it('should have property header', async () => {
+        let result = ol2json(testString, '\r\n');
 
-      expect(log).toHaveBeenCalledTimes(1);
-      expect(log).toHaveBeenCalledWith("Hello World 👋");
-    } finally {
-      console.log = _;
-    }
-  });
+        expect(result).toHaveProperty('header');
+    });
+    it('should have property 1', async () => {
+        let result = ol2json(testString, '\r\n');
+        expect(result).toHaveProperty('1');
+    });
+    it('should have property 1.2', async () => {
+        let result = ol2json(testString, '\r\n');
+        expect(result['1']).toHaveProperty('2');
+    });
+    it('should have property 1.1.2', async () => {
+        let result = ol2json(testString, '\r\n');
+        expect(result['1']['1']).toHaveProperty('2');
+    });
+    it('should have property 2', async () => {
+        let result = ol2json(testString, '\r\n');
+        expect(result).toHaveProperty('2');
+    });
+
+    it('should have "hose being" in substring of property header', async () => {
+        let result = ol2json(testString, '\r\n');
+        expect(result.header).toContain('hose being');
+    });
+    it('should have "re unnecessary clicking per sear" in substring of property 1.3', async () => {
+        let result = ol2json(testString, '\r\n');
+        expect(result['1']['3'].text).toContain('re unnecessary clicking per sear');
+    });
 });
